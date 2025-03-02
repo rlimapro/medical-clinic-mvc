420fc0648abb0c59a47c08ff8217e07f68ea206d
Rian Lima <rlimamendes@proton.me>
2025-03-02 18:07:59 -0300

    project created

---
+++ src/main/java/com/mballem/curso/security/web/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.mballem.curso.security.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+	// abrir pagina home
+	@GetMapping({"/", "/home"})
+	public String home() {
+		return "home";
+	}	
+}
