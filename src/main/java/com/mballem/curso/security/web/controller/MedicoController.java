78c3a0776656b4474a4eecc9994b3a22d76b511c
Rian Lima <rlimamendes@proton.me>
2025-03-08 00:11:53 -0300

    update method to create a doctor by doctor user

--- src/main/java/com/mballem/curso/security/web/controller/MedicoController.java
+++ src/main/java/com/mballem/curso/security/web/controller/MedicoController.java
@@ -1,8 +1,12 @@
 package com.mballem.curso.security.web.controller;
 
 import com.mballem.curso.security.domain.Medico;
+import com.mballem.curso.security.domain.Usuario;
 import com.mballem.curso.security.service.MedicoService;
+import com.mballem.curso.security.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +20,8 @@ public class MedicoController {
 
     @Autowired
     private MedicoService medicoService;
+    @Autowired
+    private UsuarioService usuarioService;
 
     // abrir pagina de dados pessoais de medicos pelo MEDICO
     @GetMapping("/dados")
@@ -24,7 +30,12 @@ public class MedicoController {
     }
 
     @PostMapping("/salvar")
-    public String salvar(Medico medico, RedirectAttributes attributes) {
+    public String salvar(Medico medico, RedirectAttributes attributes, @AuthenticationPrincipal User user) {
+        // Se o metodo for chamado por um MEDICO
+        if(medico.hasNotId() && medico.getUsuario().hasNotId()) {
+            Usuario usuario = usuarioService.buscarPorEmail(user.getUsername());
+            medico.setUsuario(usuario);
+        }
         medicoService.salvar(medico);
         attributes.addFlashAttribute("sucesso", "Operação realizada com sucesso!");
         attributes.addFlashAttribute("medico", medico);
